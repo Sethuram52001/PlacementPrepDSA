5d5a14bec341cfa5d35d170c948065c617e3a8c1
Sethuram.S.V <sethuram52001@gmail.com>
2021-11-22 06:34:22 +0530

    updated my status in the sde sheet

--- arrays/problems/SortColors.java
+++ arrays/problems/SortColors.java
@@ -2,6 +2,17 @@ package problems;
 
 import java.util.Arrays;
 
+/*
+Question:
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, 
+with the colors in the order red, white, and blue.
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+Solution:
+Dutch National Flag algo - maintain 3 pointers - l, m, h
+left side of l must be 0 and right side of h should be 1
+*/
+
 public class SortColors {
     private static void sortColors(int[] nums) {
         int l = 0, h = nums.length - 1, mid = 0;
