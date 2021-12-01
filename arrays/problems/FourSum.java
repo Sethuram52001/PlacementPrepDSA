418482d009cfe18bfd7fe05087f6e92890ae29b9
Sethuram.S.V <sethuram52001@gmail.com>
2021-12-01 05:57:33 +0530

    solved the 4 sum using sorting and 2 pointer approach

---
+++ arrays/problems/FourSum.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.*;
+
+/*
+ Problem:
+    Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+    i. 0 <= a, b, c, d < n
+    ii. a, b, c, and d are distinct.
+    iii. nums[a] + nums[b] + nums[c] + nums[d] == target
+
+    You may return the answer in any order.
+ 
+ Link: https://leetcode.com/problems/4sum/
+
+ Solution:
+ first sort the array
+ now use 2 for loops fix 1st and 2nd number of the quad pair
+ now use 2 pointer approach to find the 3rd and 4th number of the quad pair
+ 
+*/
+
+public class FourSum {
+    
+}
