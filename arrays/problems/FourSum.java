477542ee0e9d316d066cd22237d364191a777a1c
Sethuram.S.V <sethuram52001@gmail.com>
2021-12-01 06:00:30 +0530

    solved the 4 sum problem using sorting and 2 pointer approach

--- arrays/problems/FourSum.java
+++ arrays/problems/FourSum.java
@@ -22,5 +22,61 @@ import java.util.*;
 */
 
 public class FourSum {
-    
+    private static List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 3; i++) {
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                int rem = nums[i] + nums[j];
+                int l = j + 1, r = nums.length - 1;
+
+                while (l < r) {
+                    if ((rem + nums[l] + nums[r]) == target) {
+                        List<Integer> quadPair = new ArrayList<>();
+                        quadPair.add(nums[i]);
+                        quadPair.add(nums[j]);
+                        quadPair.add(nums[l]);
+                        quadPair.add(nums[r]);
+                        res.add(quadPair);
+
+                        // skipping 3rd number if it's duplicate
+                        while (l < r && nums[l] == quadPair.get(2)) {
+                            l++;
+                        }
+
+                        //skipping 4th number if it's duplicate
+                        while (l < r && nums[r] == quadPair.get(3)) {
+                            r--;
+                        }
+                    } else if ((rem + nums[l] + nums[r]) < target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+
+                // skipping 2nd number if it's duplicate
+                while (j + 1 < nums.length && nums[j + 1] == nums[j]) {
+                    j++;
+                }
+            }
+            // skipping 2nd number if it's duplicate
+            while (i + 1 < nums.length && nums[i + 1] == nums[i]) {
+                i++;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 1, 0, -1, 0, -2, 2 };
+        int target = 0;
+        List<List<Integer>> res = fourSum(nums, target);
+
+        for (List<Integer> quadPair : res) {
+            System.out.println(quadPair.toString());
+        }
+    }
 }
