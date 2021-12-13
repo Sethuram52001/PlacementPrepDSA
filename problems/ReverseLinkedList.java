48b936621cfb7e4e452364c21912bed3330ee6b9
Sethuram.S.V <sethuram52001@gmail.com>
2021-12-13 12:01:27 +0530

    solved the reverse a linked list problem

---
+++ problems/ReverseLinkedList.java
@@ -0,0 +1,58 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList {
+    private static ListNode reverseRec(ListNode head) {
+        return reverseRec(head, null);
+    }
+
+    private static ListNode reverseRec(ListNode curr, ListNode prev) {
+        if (curr == null) {
+            return prev;
+        }
+
+        ListNode next = curr.next;
+        curr.next = prev;
+        return reverseRec(next, curr);
+    }
+
+    private static ListNode reverseIt(ListNode head) {
+        ListNode prev = null, curr = head, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = null;
+
+        ListNode reversedHead = reverseRec(head);
+        while (reversedHead != null) {
+            System.out.print(reversedHead.val + " ");
+            reversedHead = reversedHead.next;
+        }
+        System.out.println(); 
+    }
+}
