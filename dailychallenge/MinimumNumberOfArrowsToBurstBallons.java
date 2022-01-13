783ac147c50a27fe257b5419c38cc6254df6b0d3
Sethuram.S.V <sethuram52001@gmail.com>
2022-01-13 21:52:53 +0530

    solved minimum number of arrows to burst ballons

---
+++ dailychallenge/MinimumNumberOfArrowsToBurstBallons.java
@@ -0,0 +1,37 @@
+/*
+Problem:
+There are some spherical balloons taped onto a flat wall that represents the XY-plane. The balloons are represented as a 2D integer 
+array points where points[i] = [xstart, xend] denotes a balloon whose horizontal diameter stretches between xstart and xend. You do 
+not know the exact y-coordinates of the balloons.
+
+Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis. A balloon with xstart 
+and xend is burst by an arrow shot at x if xstart <= x <= xend. There is no limit to the number of arrows that can be shot. A shot arrow 
+keeps traveling up infinitely, bursting any balloons in its path.
+
+Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+
+Link: https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+
+Solution:
+Greedy soln - sort the array according xend positions 
+*/
+
+import java.util.*;
+
+public class MinimumNumberOfArrowsToBurstBallons {
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        int noOfArrows = 1;
+        int arrowPos = points[0][1];
+
+        for (int i = 1; i < points.length; i++) {
+            if (arrowPos >= points[i][0]) {
+                continue;
+            }
+            arrowPos = points[i][1];
+            noOfArrows++;
+        }
+
+        return noOfArrows;
+    }
+}
