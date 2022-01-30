fc675a20d8d6beb1806ed6b67377ad002242272e
Sethuram.S.V <sethuram52001@gmail.com>
2022-01-30 10:54:28 +0530

    solved rotate array using rotate reverse algo

---
+++ dailychallenge/RotateArray.java
@@ -0,0 +1,29 @@
+/*
+Problem:
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Link: https://leetcode.com/problems/rotate-array/
+
+Solution:
+We can reverse rotate algorithm
+*/
+
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
