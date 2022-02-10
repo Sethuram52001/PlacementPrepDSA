ac3bac1d3eb603625932169f019935b99bd81044
Sethuram.S.V <sethuram52001@gmail.com>
2022-02-10 16:36:54 +0530

    solved subarray sum equals k using hashmap and prefix array concepts

---
+++ dailychallenge/SubarraySumEqualsK.java
@@ -0,0 +1,51 @@
+/*
+Problem:
+Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
+
+Link: https://leetcode.com/problems/subarray-sum-equals-k/
+
+Solution:
+We can cache in the continuous sum and look for the occurence of currSum - k in the hash table.
+Since we know that if currSum - k = x has occured earlier then we can say that there is a continous subarray of sum equals k between 
+x's occurence and current index.
+*/
+
+import java.util.*;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length, count = 0, currSum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int i = 0; i < n; i++) {
+            currSum += nums[i];
+
+            if (map.containsKey(currSum - k)) {
+                count += map.get(currSum - k);
+            }
+
+            map.put(currSum, map.getOrDefault(currSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public int subarraySum_(int[] nums, int k) {
+        int n = nums.length;
+        int[] pre = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            pre[i] = pre[i - 1] + nums[i - 1];
+        }
+
+        int count = 0;
+        for (int i = 0; i <= n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (pre[j] - pre[i] == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
