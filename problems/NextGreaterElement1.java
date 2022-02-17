7f83b2d207e88e8baf54cfc9f75dccfa4319fbe3
Sethuram.S.V <sethuram52001@gmail.com>
2022-02-17 16:30:43 +0530

    added the other version of this problem which has only 1 array

--- problems/NextGreaterElement1.java
+++ problems/NextGreaterElement1.java
@@ -8,7 +8,12 @@ For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] 
 
 Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
 
-Link: https://leetcode.com/problems/next-greater-element-i/
+Another version:
+There is only array, perform the same task on the array.
+
+Link: 
+i. 2 arrays: https://leetcode.com/problems/next-greater-element-i/
+ii. 1 array: https://www.codingninjas.com/codestudio/problems/799354?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=1
 
 Solution:
 We can use a stack and hashmap to cache in the greater element of particular element of the 2nd array.
@@ -32,4 +37,19 @@ public class NextGreaterElement1 {
         
         return nums1;
     }   
+
+    public static int[] nextGreater(int[] arr, int n) {	
+		//Write Your code here
+		Stack<Integer> st = new Stack<>();
+        for(int i = n - 1; i >= 0; i--) {
+            //System.out.println(st.toString());
+            while(!st.isEmpty() && st.peek() <= arr[i]) {
+                st.pop();
+            }
+            int val = arr[i];
+            arr[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(val);
+        }
+        return arr;
+    }
 }
