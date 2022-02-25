1cf41a9198bfc84862c070699be88022b3bbeb1a
Sethuram.S.V <sethuram52001@gmail.com>
2022-02-25 19:14:46 +0530

    solved compare version numbers

---
+++ dailychallenge/CompareVersionNumbers.java
@@ -0,0 +1,36 @@
+/*
+Problem:
+Given two version numbers, version1 and version2, compare them.
+
+Version numbers consist of one or more revisions joined by a dot '.'. Each revision consists of digits and may contain leading zeros. 
+Every revision contains at least one character. Revisions are 0-indexed from left to right, with the leftmost revision being revision 0, 
+the next revision being revision 1, and so on. For example 2.5.33 and 0.1 are valid version numbers.
+
+To compare version numbers, compare their revisions in left-to-right order. Revisions are compared using their integer value ignoring any 
+leading zeros. This means that revisions 1 and 001 are considered equal. If a version number does not specify a revision at an index, then 
+treat the revision as 0. For example, version 1.0 is less than version 1.1 because their revision 0s are the same, but their revision 1s 
+are 0 and 1 respectively, and 0 < 1.
+
+Link: https://leetcode.com/problems/compare-version-numbers/
+
+Solution:
+We can just do 1 pass iteration with Integer.compareTo(Integer).
+*/
+
+public class CompareVersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        String[] levels1 = version1.split("\\.");
+        String[] levels2 = version2.split("\\.");
+        
+        int len = Math.max(levels1.length, levels2.length);
+        for(int i = 0; i < len; i++) {
+            Integer val1 = i >= levels1.length ? 0 : Integer.valueOf(levels1[i]);
+            Integer val2 = i >= levels2.length ? 0 : Integer.valueOf(levels2[i]);
+            int compare = val1.compareTo(val2);
+            if(compare != 0) {
+                return compare;
+            }
+        }
+        return 0;
+    }   
+}
