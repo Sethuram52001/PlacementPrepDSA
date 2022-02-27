03abc8642f2631e705331ebe65b49d14f3ea5693
Sethuram52001 <sethuram52001@gmail.com>
2022-02-27 18:31:58 +0530

    solved ransome note using freq map

---
+++ problems/RansomNote.java
@@ -0,0 +1,14 @@
+/*
+Problem:
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+Each letter in magazine can only be used once in ransomNote.
+
+Link: https://leetcode.com/problems/ransom-note/
+
+Solution:
+freq map using array
+*/
+
+public class RansomNote {
+
+}
