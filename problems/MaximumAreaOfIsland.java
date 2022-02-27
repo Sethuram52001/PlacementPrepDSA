8a0210b8df2e83afc896ff95b08dd81d469cba2b
Sethuram52001 <sethuram52001@gmail.com>
2022-02-27 18:02:49 +0530

    solved maximum area of island using dfs

---
+++ problems/MaximumAreaOfIsland.java
@@ -0,0 +1,40 @@
+/*
+Problem:
+You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) 
+You may assume all four edges of the grid are surrounded by water.
+
+The area of an island is the number of cells with a value 1 in the island.
+
+Return the maximum area of an island in grid. If there is no island, return 0.
+
+Link: https://leetcode.com/problems/max-area-of-island/
+
+Solution:
+DFS
+*/
+
+public class MaximumAreaOfIsland {
+    boolean[][] visited;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int r, int c) {
+        if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || visited[r][c] || grid[r][c] == 0) {
+            return 0;
+        }
+        visited[r][c] = true;
+        return 1 + dfs(grid, r + 1, c) + dfs(grid, r - 1, c) + dfs(grid, r, c + 1) + dfs(grid, r, c - 1);
+    }    
+}
