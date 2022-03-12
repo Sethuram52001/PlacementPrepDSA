47e8e2c005c02194e80cee517ce2ad5aa02ec1e4
Sethuram.S.V <sethuram52001@gmail.com>
2022-03-12 11:20:40 +0530

    solved product of array except self using prefix and postfix product array

---
+++ problems/ProductOfArrayExceptSelf.java
@@ -0,0 +1,39 @@
+/*
+Problem:
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all 
+the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Link: https://leetcode.com/problems/product-of-array-except-self/
+
+Solution:
+prefix and postfix array of products
+*/
+
+public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] pre = new int[n];
+        int[] post = new int[n];
+        
+        pre[0] = 1;
+        for(int i = 1; i < nums.length; i++) {
+            pre[i] = pre[i - 1]*nums[i - 1];
+        }
+        
+        post[n - 1] = 1;
+        for(int i =  n - 2; i >= 0; i--) {
+            post[i] = post[i + 1]*nums[i + 1];
+        }
+        
+        int[] res = new int[n];
+        for(int i = 0; i < n; i++) {
+            res[i] = pre[i]*post[i];
+        }
+        
+        return res;
+    }   
+}
