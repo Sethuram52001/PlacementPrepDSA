797044528f6fcad5020b72db445d9d06b4004216
Sethuram.S.V <sethuram52001@gmail.com>
2022-03-12 11:27:46 +0530

    added constant space solution

--- problems/ProductOfArrayExceptSelf.java
+++ problems/ProductOfArrayExceptSelf.java
@@ -14,6 +14,24 @@ prefix and postfix array of products
 */
 
 public class ProductOfArrayExceptSelf {
+    public int[] productExceptSelf_ConstantSpace(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        res[0] = 1;
+        for (int i = 1; i < n; i++) {
+            res[i] = nums[i - 1] * res[i - 1];
+        }
+
+        int postProduct = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= postProduct;
+            postProduct *= nums[i];
+        }
+
+        return res;
+    }
+    
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] pre = new int[n];
