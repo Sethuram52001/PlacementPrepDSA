d455b80982c65a1ab2e62476f5287e9cf5507c09
Sethuram.S.V <sethuram52001@gmail.com>
2022-03-28 22:07:22 +0530

    solved search in sorted array 2 using binary search

---
+++ dailychallenge/SearchInSortedArray2.java
@@ -0,0 +1,51 @@
+/*
+Problem:
+There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is 
+[nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at 
+pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+
+Link: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+Solution:
+Binary search
+*/
+
+public class SearchInSortedArray2 {
+    public boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1, mid = -1;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            //If we know for sure right side is sorted or left side is unsorted
+            if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+                //If we know for sure left side is sorted or right side is unsorted
+            } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+                //If we get here, that means nums[start] == nums[mid] == nums[end], then shifting out
+                //any of the two sides won't change the result but can help remove duplicate from
+                //consideration, here we just use end-- but left++ works too
+            } else {
+                end--;
+            }
+        }
+
+        return false;
+    }
+}
