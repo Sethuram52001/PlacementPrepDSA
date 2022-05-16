c1760b3bca4a38f8707580fc3ca5632b881333a3
Sethuram.S.V <sethuram52001@gmail.com>
2022-05-16 15:07:08 +0530

    added dp-tabulation for decode ways

--- problems/DecodeWays.java
+++ problems/DecodeWays.java
@@ -31,6 +31,24 @@ import java.util.*;
 public class DecodeWays {
     HashMap<Integer, Integer> map;
 
+    public int numDecodings_Tabulation(String s) {
+        int[] dp = new int[s.length() + 1];
+        dp[s.length()] = 1;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == '0') {
+                dp[i] = 0;
+                continue;
+            }
+            dp[i] = dp[i + 1];
+            if (i <= s.length() - 2 && Integer.parseInt(s.substring(i, i + 2)) <= 26) {
+                dp[i] += dp[i + 2];
+            }
+        }
+
+        return dp[0];
+    }
+
     public int numDecodings(String s) {
         map = new HashMap<>();
         return numDecodings(s, 0);
