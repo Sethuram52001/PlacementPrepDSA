20242b6b59bfad0e087b5279ff8754f32dead431
Sethuram.S.V <sethuram52001@gmail.com>
2022-05-17 21:27:28 +0530

    solved longest common subsequence using dp

---
+++ problems/LongestCommonSubsequence.java
@@ -0,0 +1,38 @@
+/*
+Problem:
+Given two strings text1 and text2, return the length of their longest common subsequence. 
+If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters 
+(can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+Link: https://leetcode.com/problems/longest-common-subsequence/
+
+Solution:
+DP -  DP[i][j] represents the longest common subsequence of text1[0 ... i] & text2[0 ... j].
+DP[i][j] = DP[i - 1][j - 1] + 1 , if text1[i] == text2[j] DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]) , otherwise.
+*/
+
+public class LongestCommonSubsequence {
+    public int longestCommonSubsequence(String text1, String text2) {
+        if (text1.length() < text2.length()) {
+            return longestCommonSubsequence(text2, text1);
+        }
+        int m = text1.length(), n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
