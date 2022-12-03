6a9c63273e09650dcb85a2660e1fc921b25fad20
Sethuram.S.V <sethuram52001@gmail.com>
2022-12-03 11:01:28 +0530

    Added bucket sort approach

--- problems/SortCharactersByFrequency.java
+++ problems/SortCharactersByFrequency.java
@@ -11,7 +11,40 @@ Solution:
 We can use a hashmap as a frequency map and priority queue to sort it.
 */
 
+import java.util.*;
+
 public class SortCharactersByFrequency {
+    public String frequencySort_NoPQ(String s) {
+        int len = s.length();
+        List<Character> buckets[] = new ArrayList[len + 1];
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        for (char ch : s.toCharArray()) {
+            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : freqMap.keySet()) {
+            int freq = freqMap.get(ch);
+            if (buckets[freq] == null) {
+                buckets[freq] = new ArrayList<>();
+            }
+            buckets[freq].add(ch);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int freq = len; freq > 0; freq--) {
+            if (buckets[freq] == null) {
+                continue;
+            }
+
+            for (Character ch : buckets[freq]) {
+                sb.append(String.valueOf(ch).repeat(freq));
+            }
+        }
+
+        return sb.toString();
+    }
+    
     public String frequencySort(String s) {
         HashMap<Character, Integer> freqMap = new HashMap<>();
         for(int i = 0; i < s.length(); i++) {
