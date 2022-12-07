5378137cdc03e58c03fe703e7fd2192c414c68e5
Sethuram.S.V <sethuram52001@gmail.com>
2022-12-07 10:13:04 +0530

    Added iterative approach to range sum bst

--- dailychallenge/RangeSumBST.java
+++ dailychallenge/RangeSumBST.java
@@ -8,7 +8,7 @@ Link: https://leetcode.com/problems/range-sum-of-bst/
 Solution:
 We can follow a recursive approach, we know that left leaf is less than root node and right node is more than root node in a BST
 */
-
+import java.util.Stack;
 class TreeNode {
     int val;
     TreeNode left;
@@ -25,22 +25,48 @@ class TreeNode {
 public class RangeSumBST {
     public int rangeSumBST(TreeNode root, int low, int high) {
         int sum = 0;
-        if(root == null) {
+        if (root == null) {
             return sum;
         }
-        
-        if(root.val >= low && root.val <= high) {
+
+        if (root.val >= low && root.val <= high) {
             sum += root.val;
         }
-        
-        if(root.val > high) {
+
+        if (root.val > high) {
             return rangeSumBST(root.left, low, high);
         }
-        
-        if(root.val < low) {
+
+        if (root.val < low) {
             return rangeSumBST(root.right, low, high);
         }
-        
+
         return sum + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+    
+    public int rangeSumBST_Iterative(TreeNode root, int low, int high) {
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        int sum = 0;
+
+        while(!st.isEmpty()) {
+            TreeNode curr = st.pop();
+            if(curr == null) {
+                continue;
+            }
+
+            if(curr.val > low) {
+                st.push(curr.left);
+            }
+            if(curr.val < high) {
+                st.push(curr.right);
+            }
+
+            if(low <= curr.val && curr.val <= high) {
+                sum += curr.val;
+            }
+        }
+
+        return sum;
     }
 }
